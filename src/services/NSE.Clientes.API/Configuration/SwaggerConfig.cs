a125b4b412a3c38a6928d65f4dccca04994c1f50
Léo <ldos_rj2008@hotmail.com>
2021-11-21 04:40:07 -0300

    Aula 05.

--- src/services/NSE.Clientes.API/Configuration/SwaggerConfig.cs
+++ src/services/NSE.Clientes.API/Configuration/SwaggerConfig.cs
@@ -3,7 +3,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Models;
 using System;
 
-namespace NSE.Catalogo.API.Configuration
+namespace NSE.Clientes.API.Configuration
 {
     public static class SwaggerConfig
     {
@@ -13,7 +13,7 @@ namespace NSE.Catalogo.API.Configuration
             {
                 c.SwaggerDoc("v1", new OpenApiInfo()
                 {
-                    Title = "NerdStore Enterprise Catálogo API",
+                    Title = "NerdStore Enterprise Clientes API",
                     Description = "Esta API faz parte do curso ASP.NET Core Enterprise Applications.",
                     Contact = new OpenApiContact() { Name = "Leonardo Dias", Email = "Ldosrj2010@gmail.com" },
                     License = new OpenApiLicense() { Name = "MIT", Url = new Uri("https://opensource.org/licenses/MIT") }
@@ -43,6 +43,7 @@ namespace NSE.Catalogo.API.Configuration
                         new string[] {}
                     }
                 });
+
             });
         }
 
