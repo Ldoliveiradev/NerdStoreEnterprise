eaccf88045c31d03de65d68ae70277fc3b84aa4e
Léo <ldos_rj2008@hotmail.com>
2021-11-22 00:34:21 -0300

    Aula 01.

---
+++ src/services/NSE.Carrinho.API/Configuration/DependencyInjectionConfig.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using NSE.Carrinho.API.Data;
+
+namespace NSE.Carrinho.API.Configuration
+{
+    public static class DependencyInjectionConfig
+    {
+        public static void RegisterServices(this IServiceCollection services)
+        {
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddScoped<CarrinhoContext>();
+        }
+    }
+}
---
+++ src/services/NSE.Carrinho.API/Data/CarrinhoContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NSE.Carrinho.API.Data
+{
+    public sealed class CarrinhoContext : DbContext
+    {
+        public CarrinhoContext(DbContextOptions<CarrinhoContext> options)
+            : base(options)
+        {
+            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+            ChangeTracker.AutoDetectChangesEnabled = false;
+        }
+    }
+}
