c4f5e876e60c42d9a1edb5c78ed9b2c85eff6911
Léo <ldos_rj2008@hotmail.com>
2021-11-23 13:51:04 -0300

    Aula 03.

---
+++ src/services/NSE.Pagamentos.API/Data/PagamentosContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NSE.Pagamentos.API.Data
+{
+    public sealed class PagamentosContext : DbContext
+    {
+        public PagamentosContext(DbContextOptions<PagamentosContext> options) : base(options)
+        {
+            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+            ChangeTracker.AutoDetectChangesEnabled = false;
+        }
+    }
+}
---
+++ src/services/NSE.Pagamentos.API/Services/PagamentoIntegrationHandler.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Hosting;
+using NSE.MessageBus;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NSE.Pagamentos.API.Services
+{
+    public class PagamentoIntegrationHandler : BackgroundService
+    {
+        private readonly IMessageBus _bus;
+        private readonly IServiceProvider _serviceProvider;
+
+        public PagamentoIntegrationHandler(IServiceProvider serviceProvider, IMessageBus bus)
+        {
+            _serviceProvider = serviceProvider;
+            _bus = bus;
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
