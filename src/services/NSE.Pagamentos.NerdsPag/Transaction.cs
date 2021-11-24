93e04c9b8073cf482fa48f13e349fa47e3398bf5
Léo <ldos_rj2008@hotmail.com>
2021-11-24 03:01:55 -0300

    Aula 13.

--- src/services/NSE.Pagamentos.NerdsPag/Transaction.cs
+++ src/services/NSE.Pagamentos.NerdsPag/Transaction.cs
@@ -16,81 +16,43 @@ namespace NSE.Pagamentos.NerdsPag
         private readonly NerdsPagService NerdsPagService;
 
         protected string Endpoint { get; set; }
-
         public int SubscriptionId { get; set; }
-
         public TransactionStatus Status { get; set; }
-
         public int AuthorizationAmount { get; set; }
-
         public int PaidAmount { get; set; }
-
         public int RefundedAmount { get; set; }
-
         public string CardHash { get; set; }
-
         public string CardNumber { get; set; }
-
         public string CardExpirationDate { get; set; }
-
         public string StatusReason { get; set; }
-
         public string AcquirerResponseCode { get; set; }
-
         public string AcquirerName { get; set; }
-
         public string AuthorizationCode { get; set; }
-
         public string SoftDescriptor { get; set; }
-
         public string RefuseReason { get; set; }
-
         public string Tid { get; set; }
-
         public string Nsu { get; set; }
-
         public decimal Amount { get; set; }
-
         public int? Installments { get; set; }
-
         public decimal Cost { get; set; }
-
         public string CardHolderName { get; set; }
-
         public string CardCvv { get; set; }
-
         public string CardLastDigits { get; set; }
-
         public string CardFirstDigits { get; set; }
-
         public string CardBrand { get; set; }
-
         public string CardEmvResponse { get; set; }
-
         public string PostbackUrl { get; set; }
-
         public PaymentMethod PaymentMethod { get; set; }
-
         public float? AntifraudScore { get; set; }
-
         public string BilletUrl { get; set; }
-
         public string BilletInstructions { get; set; }
-
         public DateTime? BilletExpirationDate { get; set; }
-
         public string BilletBarcode { get; set; }
-
         public string Referer { get; set; }
-
         public string IP { get; set; }
-
         public bool? ShouldCapture { get; set; }
-
         public bool? Async { get; set; }
-
         public string LocalTime { get; set; }
-
         public DateTime TransactionDate { get; set; }
 
         public Task<Transaction> AuthorizeCardTransaction()
