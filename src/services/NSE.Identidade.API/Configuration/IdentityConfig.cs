a64242531fa5af500d64e7e1f25f029599b932fb
Léo <ldos_rj2008@hotmail.com>
2021-11-25 11:03:22 -0300

    Aula 08.

--- src/services/NSE.Identidade.API/Configuration/IdentityConfig.cs
+++ src/services/NSE.Identidade.API/Configuration/IdentityConfig.cs
@@ -2,6 +2,8 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using NetDevPack.Security.Jwt;
+using NetDevPack.Security.Jwt.Store.EntityFrameworkCore;
 using NSE.Identidade.API.Data;
 using NSE.Identidade.API.Extensions;
 using NSE.WebAPI.Core.Identidade;
@@ -12,6 +14,9 @@ namespace NSE.Identidade.API.Configuration
     {
         public static IServiceCollection AddIdentityConfiguration(this IServiceCollection services, IConfiguration configuration)
         {
+            services.AddJwksManager(options => options.Jws = JwsAlgorithm.ES256)
+                .PersistKeysToDatabaseStore<ApplicationDbContext>();
+
             services.AddDbContext<ApplicationDbContext>(options =>
                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
 
